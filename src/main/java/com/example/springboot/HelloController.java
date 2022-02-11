667e8ec5f28d0d057fd1ef09e9b71f1321e9ef35
benwilcock <benwilcock@gmail.com>
2022-02-11 15:55:28 +0000

    Added REST endpoint

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,22 +1,34 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
+	@Value("${spring.application.name:tanzu-java-web-app}")
+    String appName;
+
 	@RequestMapping("/")
-	public String index() {
-		StringBuffer buf = new StringBuffer();
+	public Map<String, String> index() {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
 
-		buf.append("Greetings from Ben, VMware, Spring Boot, and Tanzu!");
-		buf.append("<br>");
-		buf.append("The time is: " + dtf.format(now));
-		return buf.toString();
+		LOG.info("A request has been received for the /rest endpoint.");
+		Map<String, String> data = new HashMap<String, String>();
+		data.put("Application Name:", appName);
+		data.put("Greetings From:", "Ben, VMware, Spring Boot, and Tanzu!");
+		data.put("The Date & Time:", dtf.format(now));
+		LOG.debug("Returning {}.", data.toString());
+		return data;
 	}
 }
