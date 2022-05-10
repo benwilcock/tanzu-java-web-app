98be71a0c4828dc195abd05f0ce39ff9a2141b25
benwilcock <benwilcock@gmail.com>
2022-05-10 15:25:21 +0100

    changed logs. added back security config.

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,11 +2,12 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 // import org.springframework.context.annotation.Configuration;
 // import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 // import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,31 +21,31 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+	// @Bean
+	// public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	// 	return args -> {
 
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
+	// 		String[] beanNames = ctx.getBeanDefinitionNames();
+	// 		Arrays.sort(beanNames);
+	// 		System.out.println("Let's inspect the beans provided by Spring Boot:");
+	// 		for (String beanName : beanNames) {
+	// 			System.out.println(beanName);
+	// 		}
 
-		};
-	}
+	// 	};
+	// }
 
 	@Bean
 	public HttpTraceRepository htttpTraceRepository() {
 		return new InMemoryHttpTraceRepository();
 	}
 
-	// @Configuration
-	// public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-	// 	@Override
-	// 	protected void configure(HttpSecurity http) throws Exception {
-	// 		http.authorizeRequests().anyRequest().permitAll()
-	// 				.and().csrf().disable();
-	// 	}
-	// }
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
+	}
 }
